b99110413b68147e1fb090db40978bee8cadc7f2
rutuja3562 <rspatil3562@gmail.com>
2022-08-30 23:03:03 +0530

    update

--- deltax-backend/src/configs/db.js
+++ deltax-backend/src/configs/db.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
   return mongoose.connect(
-    // "mongodb+srv://dhaval:dhaval_123@cluster0.ljuvz.mongodb.net/web15-fileuploads?retryWrites=true&w=majority"
     // "mongodb+srv://Rutu:Rutuatlas3562@cluster0.4soie.mongodb.net/Delta?retryWrites=true&w=majority"
     "mongodb://localhost:27017/?readPreference=primary&directConnection=true&ssl=false"
   );
